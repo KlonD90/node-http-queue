78509720fc579a59059cf344aacc6ed6bbcfc2b1
Nikolay Mendyaev <newbiecraft@gmail.com>
2014-12-30 01:06:35 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 var vow = require('vow');
 var IdPool = require('./lib/id_pool');
-var redis = require('./redis');
 var _ = require('underscore');
-
 
 var E = {};
 
-
-
-E.options = {
+var defaultOptions = {
     delayQueueCheck: 500,
     delayProcessedCheck: 1000,
     channelSuffix: '_default',
@@ -26,7 +22,148 @@ E.options = {
     }
 };
 
-E.pushMessage = function(options){
+function RealtimeQueue(opts){
+    this.settings = _.extend(defaultOptions, opts);
+
+}
+
+function RealtimeAgent(){
+
+}
+
+function RealtimeConsumer(){
+
+}
+
+
+/*
+    RealtimeAdapterQueue
+
+    init
+    pullTask model
+    pushTask model
+    listen
+ */
+
+function createRealtimeAdapterQueue(opts){
+    var adapter = function(channelName){
+        this.channelName = channelName;
+    };
+    adapter.prototype.pullTask = opts.pullTask;
+    adapter.prototype.pushTask = opts.pushTask;
+    adapter.prototype.listen = opts.listen;
+    adapter.prototype.init = opts.init;
+}
+
+var RealtimeRedisAdapterQueue = createRealtimeAdapterQueue({
+    initAgent: function(opts){
+        var channel = 'agent_channel_'+this.channelName+'_'+process.pid;
+        this.client = require('redis').createClient(opts);
+        this.client.on('message', function(ch, message){
+            if (channel === ch)
+            {
+                var info = JSON.parse(message);
+                resolveTask(info.data);
+                pool.info
+            }
+        });
+        this.client.subscribe('agent_channel_'+this.channelName);
+        this.idPool = new IdPool();
+        this.pool = {};
+
+    },
+    initWorker: function(opts){
+        this.client = require('redis').createClient(opts);
+        var channel = 'workers_channel_'+this.channelName;
+        this.client.on('message', function(ch){
+            if (ch==channel)
+            {
+                accquireTask();
+            }
+            //var item = pgFreeClient.getClient();
+            //logger.log(item);
+            //if (item){
+            //    redisClient.lpop(LibRecom.job_queue_name, function(err, reply){
+            //        if (reply)
+            //        {
+            //            do_work(reply, item);
+            //        }
+            //        else
+            //        {
+            //            item.done();
+            //        }
+            //    });
+            //}
+        });
+        this.client.subscribe(channel);
+    },
+    pullTask: function(){
+        var dfd = vow.defer();
+        var queueName = 'workers_channel_'+this.channelName;
+        this.client.lpop(queueName, function(err, reply){
+            if (err)
+            {
+                return void dfd.reject(err);
+            }
+            if (reply)
+            {
+                var info = JSON.parse(reply);
+                dfd.resolve([info.data, info]);
+            }
+            else
+            {
+                dfd.resolve();
+            }
+        });
+        return dfd.promise();
+    },
+    resolveTask: function(message, info){
+        var dfd = vow.defer();
+        info.data = message;
+        this.client.publish('agent_channel_'+this.channelName+'_'+info.submitter, JSON.parse(info));
+        dfd.resolve();
+        return dfd.promise();
+    },
+    pushTask: function(message){
+        var self = this;
+        var conId = this.idPool.get();
+        this.pool[conId.value()] = {
+            resource: resource,
+            con_id: conId,
+            timeout: false
+        };
+        var info = {
+            submitter: this.channelName + process.pid,
+            con_id: conId.value(),
+            data: message
+        };
+        this.client.rpush('workers_queue_'+this.channelName, JSON.stringify(info));
+        this.client.publish('workers_channel_'+this.channelName, '1');
+        this.pool[conId.value()].timeout = setTimeout(function(){
+            if (!this.pool[conId.value()])
+                return;
+            if (+reply)
+            {
+                self.pool[conId.value()] = false;
+                conId.release();
+                self.queueCheckFallback(resource);
+            }
+            else
+            {
+                self.pool[conId.value()].timeout = setTimeout(function(){
+                    if (!pool[conId.value()])
+                        return;
+                    self.pool[conId.value()] = false;
+                    conId.release();
+                    self.timeoutFallback(resource);
+                }, opt.delayProcessedCheck);
+            }
+        }, opt.delayQueueCheck);
+    }
+});
+
+
+RealtimeAgent.prototype.pushMessage = function(options){
     var pool = {};
     var opt = _.extend(E.options, options);
     opt.redisClient = opt.redisClient || redis.createClient();
@@ -59,7 +196,6 @@ E.sendMessage = function(opt, pool)
     return function(resource, message){
         var conId = pool.get();
         pool[conId.value()] = {
-            resource: resource,
             con_id: conId,
             timeout: false
         };
